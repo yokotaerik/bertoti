ffe589e8a5a5628599cbbbecf28434b59b7700e7
yokotaerik <yokota082003@gmail.com>
2024-10-04 12:09:44 -0300

    fix envio dos filmes e melhorando experiencia do usuario digitando apenas a cidade

--- ingressos/ibge.py
+++ ingressos/ibge.py
@@ -1,25 +1,24 @@
 import requests
 
-class Ibge:
-    def obter_cidades_por_estado(self, estado_id):
-        url = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{estado_id}/municipios"
-        response = requests.get(url)
-        
-        if response.status_code == 200:
-            cidades = response.json()
-            return [cidade['nome'] for cidade in cidades]
-        else:
-            return []
-
-    estados = {
-        "Acre": 12, "Alagoas": 27, "Amapá": 16, "Amazonas": 13, "Bahia": 29, "Ceará": 23,
-        "Distrito Federal": 53, "Espírito Santo": 32, "Goiás": 52, "Maranhão": 21,
-        "Mato Grosso": 51, "Mato Grosso do Sul": 50, "Minas Gerais": 31, "Pará": 15,
-        "Paraíba": 25, "Paraná": 41, "Pernambuco": 26, "Piauí": 22, "Rio de Janeiro": 33,
-        "Rio Grande do Norte": 24, "Rio Grande do Sul": 43, "Rondônia": 11, "Roraima": 14,
-        "Santa Catarina": 42, "São Paulo": 35, "Sergipe": 28, "Tocantins": 17
-    }
+def get_estados_cidades():
+    url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios"
+    response = requests.get(url)
     
+    if response.status_code == 200:
+        municipios = response.json()
+        estados_cidades = {}
 
-
-print(Ibge.obter_cidades_por_estado(Ibge,35))
+        for municipio in municipios:
+            cidade = municipio['nome']
+            estado = municipio['microrregiao']['mesorregiao']['UF']['nome']
+            
+            # Verifica se o estado já existe no dicionário, se não, cria a chave
+            if estado not in estados_cidades:
+                estados_cidades[estado] = []
+            
+            # Adiciona a cidade à lista correspondente ao estado
+            estados_cidades[estado].append(cidade)
+        
+        return estados_cidades
+    else:
+        raise Exception(f"Erro ao acessar a API do IBGE: {response.status_code}")
