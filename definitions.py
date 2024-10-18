054967f3eb4ac3da4f65e5b0aebb4fe2448aa09c
yokotaerik <yokota082003@gmail.com>
2024-10-18 09:35:09 -0300

    feat: tentando enviar foto na mensagem

--- definitions.py
+++
@@ -1,6 +0,0 @@
-################################################################################
-# Definitions
-
-SAUDACOES = ['oi', 'oie', 'Oie', 'hi', 'hello', 'ola', 'olá', 'Olá', 'Hello', 
-             'Hello', 'Hi', 'Oi', 'eae', 'Eae', 'eai', 'Eai', 'Salve', 'salve', 
-             'opa', 'Opa']
